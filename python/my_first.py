5a950fd735158f6c2c47a5a9f96a38ed5408ff9a
choikangseok <ktos5427@naver.com>
2020-04-16 14:40:29 +0900

    add_lotto

--- python/my_first.py
+++ python/my_first.py
@@ -3,18 +3,27 @@
 #      누구에게 보낼지 이름을 입력 받아서
 
 # 메일 내용을 완성해서 출력한다.
+import random
 
+def make_lotto():
+    listN=[]
+    while len(listN)<6 :
+        num = random.randint(1,46)
 
+        if num not in listN:
+            listN.append(random.randint(1,46))
+    return listN
+
+print(make_lotto())
 
 message = """안녕하세요, {0}님
 파이썬 수업에 오신걸 환영합니다
-오늘은 {1}일째 수업입니다.
+오늘의 행운의 로또 번호는 {1} 입니다.
 남은 기간 즐겁고 행복한 공부가 되길 기원합니다
 """
-who = input("누구에게 보낼까요?")
-date = input("오늘은 몇일째 수업인가요?")
+who = input("누구에게 보낼건가요?")
+data = make_lotto()
 
-complete_message = message.format(who,date)
+complete_message = message.format(who,data)
 
 print(complete_message)
-print("hellowks")
